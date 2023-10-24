5d6991f4c94426216efd850546f06e31079f05ae
diego <dgargay987@g.educaand.es>
2023-10-24 13:32:22 +0200

    Segundo commit

--- entities/user.php
+++ entities/user.php
@@ -6,39 +6,45 @@ class user {
     private $rol;
         
     //Constructor
-    public function __construct($nombre, $contraseña, $rol) {
+    public function __construct($id, $nombre, $contraseña, $rol) {
+        $this->id = $id;
         $this->nombre = $nombre;
         $this->contraseña = $contraseña;
         $this->rol = $rol;
     }
 
+    // Getter para obtener el nombre
+    public function get_Id() {
+        return $this->id;
+    }
+    
      // Getter para obtener el nombre
-     public function getNombre() {
+     public function get_Nombre() {
         return $this->nombre;
     }
 
     // Setter para establecer el nombre
-    public function setNombre($nombre) {
+    public function set_Nombre($nombre) {
         $this->nombre = $nombre;
     }
 
     // Getter para obtener la contraseña
-    public function getContraseña() {
+    public function get_Contraseña() {
         return $this->contraseña;
     }
 
     // Setter para establecer la contraseña
-    public function setContraseña($contraseña) {
+    public function set_Contraseña($contraseña) {
         $this->contraseña = $contraseña;
     }
 
     // Getter para obtener el rol
-    public function getRol() {
+    public function get_Rol() {
         return $this->rol;
     }
 
     // Setter para establecer el rol
-    public function setRol($rol) {
+    public function set_Rol($rol) {
         $this->rol = $rol;
     }
 }
