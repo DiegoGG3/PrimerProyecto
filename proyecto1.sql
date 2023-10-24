31e0dc99c7d48491ecd02914a4c736163606939f
DiegoGG3 <145332917+DiegoGG3@users.noreply.github.com>
2023-10-24 12:17:49 +0200

    Add files via upload

---
+++ proyecto1.sql
@@ -0,0 +1,61 @@
+CREATE TABLE User (
+    ID INT AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    rol VARCHAR(10),
+    PRIMARY KEY (ID)
+);
+ALTER TABLE User
+ADD CONSTRAINT chk_rol CHECK (rol IN ('profesor', 'alumno'));
+
+CREATE TABLE Examen (
+    ID INT AUTO_INCREMENT,
+    fecha_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+CREATE TABLE intento (
+	id_intento int auto_increment,
+    user_id INT,
+    examen_id INT,
+    respuestas JSON,
+	fecha_intento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    
+    PRIMARY KEY (id_intento),
+    FOREIGN KEY (user_id) REFERENCES User (ID),
+    FOREIGN KEY (examen_id) REFERENCES Examen (id)
+);
+
+create table pregunta (
+	ID_pregunta int auto_increment,
+	Enunciado varchar(200) not null,
+	respuestas JSON not null,
+	categoria varchar(20) not null,
+	dificultad varchar(20) not null,
+	URL varchar(200),
+	tipo_recurso varchar(50) ,
+
+	primary key (ID_pregunta),
+    FOREIGN KEY (categoria) REFERENCES categoria (Nombre),
+    FOREIGN KEY (dificultad) REFERENCES dificultad (Nombre)
+);
+
+create table categoria(
+	ID_categoria int auto_increment,
+    Nombre varchar(100) unique,
+    
+    primary key (ID_categoria, Nombre)
+);
+
+create table dificultad(
+	ID_dificultad int auto_increment,
+    Nombre varchar(100) unique,
+    
+    primary key (ID_dificultad, Nombre)
+);
+
+insert into categoria(ID_categoria,Nombre) values(1,"A");
+insert into dificultad(ID_dificultad,Nombre) values(1,"Facil");
+insert into pregunta(ID_pregunta,Enunciado,respuestas,categoria,dificultad) values (1,"d",'{"Pregunta1": "Respuesta1", "Pregunta2": "Respuesta2"}', "A", "Facil");
+select * from pregunta;
+commit;
