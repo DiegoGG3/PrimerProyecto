35a4a97ddcb94a336eb8a8db5a0587a9dc1cadd8
diego <dgargay987@g.educaand.es>
2023-10-30 10:03:36 +0100

    commit2

---
+++ repository/BDRepository.php
@@ -0,0 +1,19 @@
+<?php
+    function selectUniversal($conexion,$tabla){
+        $resultado = $conexion->query('SELECT * FROM '.$tabla.";", MYSQLI_USE_RESULT);
+        $objetos=array();
+        while ($registro = $resultado->fetch(PDO::FETCH_OBJ)) {
+            array_push($objetos,$registro);
+        }  
+
+        switch ($tabla) {
+            case "User":
+                return arrayUser($objetos);
+                break;
+            case "Pregunta":
+                return arrayPregunta($objetos);
+                break;
+        }
+    }
+
+?>
