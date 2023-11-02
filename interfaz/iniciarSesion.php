3782baddda8d1fef46fe6ddcb0972af595820a3d
diego <dgargay987@g.educaand.es>
2023-11-02 18:22:24 +0100

    commit

--- interfaz/iniciarSesion.php
+++ interfaz/iniciarSesion.php
@@ -37,7 +37,7 @@ if (isset($_POST['login'])) {
     <head>
         <meta charset='UTF-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <link rel='stylesheet' type='text/css' href='inicioSesio.css'>
+        <link rel='stylesheet' type='text/css' href='../css/inicioSesio.css'>
         <title>Iniciar Sesión</title>
     </head>
     <body>
