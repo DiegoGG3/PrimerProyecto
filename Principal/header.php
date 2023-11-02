4f8bcd2c603d0db58e4b0b5c7299641eecca89e2
diego <dgargay987@g.educaand.es>
2023-11-02 20:46:15 +0100

    commit login

--- Principal/header.php
+++ Principal/header.php
@@ -1,30 +1,27 @@
+
+
 <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Autoescuela Las Fuentezuelas</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="?menu=mantenimiento">Realizar Examen <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="?menu=mantenimiento">Quienes Somos<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="?menu=mantenimiento">Iniciar Sesion<span class="sr-only">(current)</span></a>
-                    </li>
-                </ul>
-                <?= Sesion::existe('login')?"Hola bienvenido ".Sesion::leer('login').
-                "<a href='?menu=cerrarsesion'>Cerrar sesión</a>":""; ?>
-
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-                </form>
-            </div>
-        </nav>
-    </header>
+    <nav class="barra-navegacion">
+        <a class="titulo-sitio" href="#">Autoescuela Las Fuentezuelas</a>
+        
+        <div class="menu-navegacion">
+            <ul class="lista-enlaces">
+                <li class="enlace">
+                    <a href="?menu=Examen">Realizar Examen <span class="oculto"></span></a>
+                </li>
+                <li class="enlace">
+                    <a href="?menu=Conocenos">Quienes Somos<span class="oculto"></span></a>
+                </li>
+                <li class="enlace">
+                    <a href="?menu=IniciaSesion">Iniciar Sesión<span class="oculto"></span></a>
+                </li>
+            </ul>
+            
+            
+            <form class="formulario-busqueda">
+                <input class="campo-busqueda" type="search" placeholder="Buscar" aria-label="Buscar">
+                <button class="boton-busqueda" type="submit">Buscar</button>
+            </form>
+        </div>
+    </nav>
+</header>
